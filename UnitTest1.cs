48d2b9a00bf0c1860453d031dcbb599cdccd70fa
ElizavetaGLZRN <157626211+ElizavetaGLZRN@users.noreply.github.com>
2024-01-24 23:47:11 +0700

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Windows.Forms;
+using Курсовая;
+
+namespace тестирование
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestButton1()
+        {
+            Form2 form = new Form2();
+            form.Button1_Click(null, null);
+
+            // Проверяем, что progressbar3.value установлено в 100
+            Assert.AreEqual(100, form.ProgressBar3.Value);
+
+            
+        }
+
+        [TestMethod]
+        public void TestButton2()
+        {
+            Form2 form = new Form2();
+            form.Button2_Click(null, null);
+
+            // Проверяем, что при значении progressbar2.value < progressbar2.maximum - 10,
+            // progressbar2.value увеличивается на 10
+            if (form.ProgressBar2.Value < form.ProgressBar2.Value - 10)
+            {
+                Assert.AreEqual(form.ProgressBar2.Value + 10, form.ProgressBar2.Value);
+            }
+
+            // Проверяем, что при значении progressbar2.value > progressbar2.maximum - 10,
+            // кнопка button2 становится неактивной
+            if (form.ProgressBar2.Value > form.ProgressBar2.Maximum - 10)
+            {
+                Assert.IsFalse(form.Button2.Enabled);
+            }
+        }
+
+        [TestMethod]
+        public void TestButton3()
+        {
+            Form2 form = new Form2();
+            form.Button3_Click(null, null);
+
+            
+
+            // Проверяем, что sleep установлено в 1
+            Assert.AreEqual(1, form.sleep);
+
+            // Проверяем, что кнопки button1 и button2 становятся неактивными
+            Assert.IsFalse(form.Button1.Enabled);
+            Assert.IsFalse(form.Button2.Enabled);
+        }
+
+        [TestMethod]
+        public void TestButton4()
+        {
+            Form2 form = new Form2();
+            form.Button4_Click(null, null);
+
+            
+
+            // Проверяем, что sleep установлено в 0
+            Assert.AreEqual(0, form.sleep);
+
+            // Проверяем, что кнопки button1 и button2 становятся активными
+            Assert.IsTrue(form.Button1.Enabled);
+            Assert.IsTrue(form.Button2.Enabled);
+        }
+    }
+}
---
+++ Игра.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace База_данных
+{
+    class База_данных
+    {
+        SqlConnection Connection = new SqlConnection(@"Data Source=DESKTOP-2E3S9D9\SQLEXPRESS; 
+Unitial Catalog=Пользователи12;Integrated Security=True");
+
+        public void OpenConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+        public void CloseConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+        public SqlConnection getConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}
+
